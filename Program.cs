51c38f9e7e1c83218e10cf4267c77b6570fc6a32
nabelly <anabellymontibeller@gmail.com>
2024-01-15 16:54:26 -0300

    fix it

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ ApplicationConfiguration.Initialize();
 
 var pb = new PictureBox { Dock = DockStyle.Fill, };
 
-var timer = new System.Windows.Forms.Timer { Interval = 20,};
+var timer = new System.Windows.Forms.Timer { Interval = 20, };
 
 Bitmap bmp = null;
 Graphics g = null;
@@ -18,12 +18,12 @@ var form = new PlayerAnimation()
     WindowState = FormWindowState.Maximized,
     FormBorderStyle = FormBorderStyle.None,
     Text = "Sprite-Test",
-    Controls = {pb}
+    Controls = { pb }
 };
 
 form.Load += (o, e) =>
 {
-    bmp =  new Bitmap(pb.Width, pb.Height);
+    bmp = new Bitmap(pb.Width, pb.Height);
     g = Graphics.FromImage(bmp);
     g.Clear(Color.Black);
     pb.Image = bmp;
@@ -34,33 +34,12 @@ form.Load += (o, e) =>
 timer.Tick += (o, e) =>
 {
     g.Clear(Color.Black);
-    if(form.goLeft && form.X > 0)
-        {
-            form.X -= form.speed;
-            form.AnimatePLayer(9,12);
-        }
-        if(form.goRight && form.X + form.width < form.ClientSize.Width)
-        {
-            form.X += form.speed;
-            form.AnimatePLayer(5,8);
-        }
-        if(form.goUp && form.Y > 0)
-        {
-            form.Y -= form.speed;
-            form.AnimatePLayer(1,4);
-        }
-        if(form.goDown && form.Y + form.height < form.ClientSize.Height)
-        {
-            form.Y += form.speed;
-            form.AnimatePLayer(13,16);
-        }
-
-        form.Draw(g);
+    form.TimerEvent(o, e);
+    form.Draw(g);
 
     pb.Refresh();
 };
-
-form.KeyUp += (o, e) =>
+form.KeyDown += (o, e) =>
 {
     switch (e.KeyCode)
     {
@@ -70,20 +49,21 @@ form.KeyUp += (o, e) =>
 
         case Keys.A:
             form.goLeft = true;
-            
+
             break;
 
         case Keys.S:
             form.goDown = true;
-            
+
             break;
 
         case Keys.D:
             form.goRight = true;
             break;
-    }
+    };
 };
-form.KeyDown += (o, e) =>
+
+form.KeyUp += (o, e) =>
 {
     switch (e.KeyCode)
     {
@@ -93,12 +73,12 @@ form.KeyDown += (o, e) =>
 
         case Keys.A:
             form.goLeft = false;
-            
+
             break;
 
         case Keys.S:
             form.goDown = false;
-            
+
             break;
 
         case Keys.D:
@@ -122,10 +102,10 @@ public class PlayerAnimation : Form
     public int width = 50;
     public int speed = 10;
 
-    
+
     public PlayerAnimation()
     {
-        
+
     }
 
     public void KeyIsDown(object sender, KeyEventArgs e)
@@ -142,7 +122,7 @@ public class PlayerAnimation : Form
 
     public void KeyIsUp(object sender, KeyEventArgs e)
     {
-         if (e.KeyCode == Keys.Left)
+        if (e.KeyCode == Keys.Left)
             goLeft = false;
         if (e.KeyCode == Keys.Right)
             goRight = false;
@@ -159,29 +139,29 @@ public class PlayerAnimation : Form
         g.DrawImage(player, X, Y, width, height);
     }
 
-    private void TimerEvent(object sender, EventArgs e)
+    public void TimerEvent(object sender, EventArgs e)
     {
-        if(goLeft && X > 0)
+        if (goLeft && X > 0)
         {
             X -= speed;
-            AnimatePLayer(9,12);
+            AnimatePLayer(5, 8);
         }
-        if(goRight && X + width < this.ClientSize.Width)
+        if (goRight && X + width < this.ClientSize.Width)
         {
             X += speed;
-            AnimatePLayer(5,8);
+            AnimatePLayer(9, 12);
         }
-        if(goUp && Y > 0)
-        {
-            Y += speed;
-            AnimatePLayer(1,4);
-        }
-        if(goDown && Y + height < this.ClientSize.Height)
+        if (goUp && Y > 0)
         {
             Y -= speed;
-            AnimatePLayer(13,16);
+            AnimatePLayer(13, 16);
         }
-      
+        if (goDown && Y + height < this.ClientSize.Height)
+        {
+            Y += speed;
+            AnimatePLayer(1, 4);
+        }
+
     }
 
     public void SetUp()
@@ -194,8 +174,8 @@ public class PlayerAnimation : Form
         player = Image.FromFile(pMove[0]);
         foreach (var item in pMove)
         {
-        MessageBox.Show(item.ToString());
-            
+            MessageBox.Show(item.ToString());
+
         }
     }
 
@@ -203,20 +183,20 @@ public class PlayerAnimation : Form
     {
         slowFrameRate += 1;
 
-        if(slowFrameRate == 4)
+        if (slowFrameRate == 4)
         {
-            steps ++;
+            steps++;
             slowFrameRate = 0;
         }
-        
-        if(steps > end || steps < start)
+
+        if (steps > end || steps < start)
         {
             steps = start;
         }
 
         player = Image.FromFile(pMove[steps]);
     }
-    public void Draw (Graphics g )
+    public void Draw(Graphics g)
     {
         g.DrawImage(player, X, Y);
     }
